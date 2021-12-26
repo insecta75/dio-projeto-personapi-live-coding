814e3dbaab9caaf98fda33151afba4d150c1fbd9
insecta75 <insecta75@yahoo.com.br>
2021-12-26 18:46:43 -0300

    Finalizado com os arquivos de testes

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,8 @@
 package one.digitalinnovation.personapi.controller;
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,24 +12,35 @@ import java.util.List;
 
 @RestController //Controlador que sera acessado por uma API REST;
 @RequestMapping("/api/v1/people") //O caminho de acesso principal da API (atende ao level 1 de Richardson);
+@AllArgsConstructor(onConstructor = @__(@Autowired)) //Para tirar o constructor padrao;
 public class PersonController {//Anotacao: ponto de entrada do projeto;
 
     private PersonService personService;
 
-    @Autowired
+    /*@Autowired
     public PersonController (PersonService personService) {
         this.personService = personService;
-    }
-
+    }*/
     @PostMapping //Mapeando uma operacao HTTP do tipo POST;
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) { //Precisa informar que vem uma requisao do tipo Pessoa;
-
         return personService.createPerson(personDTO);
     }
-
     @GetMapping //Mapeando uma operacao HTTP do tipo GET;
     public List<PersonDTO> listAll() {
         return personService.listAll();
+    }
+    @GetMapping("/{id}") //Necessario colocar para evitar conflito com o listAll;
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException { //PathVariable: necessario para o mapeamento (http para atributo do metodo);
+        return personService.findById(id);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
     }
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,25 +1,30 @@
 package one.digitalinnovation.personapi.service;
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service /*Indica para gerenciar uma classe, que sera responsavel
 por colocar todas as regras de negocios na aplicacao;*/
+@AllArgsConstructor(onConstructor = @__(@Autowired)) //Para tirar o constructor padrao;
 public class PersonService { //Regra de criacao delegada para essa classe de Service;
     private PersonRepository personRepository;
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-    @Autowired
+    /*@Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
-    }
+    }*/
     public MessageResponseDTO createPerson(PersonDTO personDTO) { //Precisa informar que vem uma requisao do tipo Pessoa;
         Person personToSave = personMapper.toModel(personDTO); //Codigo abaixo é igual a isso;
         /*Person personToSave = Person.builder()
@@ -28,12 +33,8 @@ public class PersonService { //Regra de criacao delegada para essa classe de Ser
                                     .birthDate(personDTO.getBirthDate())
                                     .phones(personDTO.getPhones())
                                     .build();*/
-
         Person savedPerson = personRepository.save(personToSave); //Primeira operacao: criacao de usuarios;
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID "+savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Created person with ID "); // Trecho refatorado;
     }
 
     public List<PersonDTO> listAll() {
@@ -43,5 +44,35 @@ public class PersonService { //Regra de criacao delegada para essa classe de Ser
         return allPeople.stream()
                 .map(personMapper::toDTO) //converte cada linha do allPeople em um DTO;
                 .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = verifyIfExists(id);
+        return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id); // Mesma operacao de criacao, mas com a verificacao se o Id ja existe;
+        Person personToUpdate = personMapper.toModel(personDTO);
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(updatedPerson.getId(), "Updated person with ID "); // Trecho refatorado;
+    }
+    // Primeiros os metodos publicos, e no final os privados;
+    private Person verifyIfExists(Long id) throws PersonNotFoundException { //Evitar codigo repetitivo;
+        return personRepository
+                .findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
     }
 }
